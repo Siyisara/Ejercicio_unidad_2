e59d510cc2202db2f5275cea92181037c42d74cd
Siyisara <68314871+Siyisara@users.noreply.github.com>
2020-07-28 19:41:45 -0500

    Update APP.java

--- ejercicio2/src/main/java/Interfaz/APP.java
+++ ejercicio2/src/main/java/Interfaz/APP.java
@@ -10,74 +10,123 @@ public class APP {
 		// TODO Auto-generated method stub
 		Principal p = new Principal();
 		
-		int numero = 0;
-		while(numero != 5) {
-			numero = Integer.parseInt(JOptionPane.showInputDialog("Digite lo que desea" + "\n" + "1: Agregar Empleado"
-					+ "\n" + "2: Eliminar empleado" + "\n" + "3: Modificar Empleado" + "\n" + "4: Mostrar Empleados" + "\n" + "5: Salir"));
-			
-			switch (numero) {
-			case 1:
-				try {
-					int id = Integer.parseInt(JOptionPane.showInputDialog("Digite id"));
-					String nombre = JOptionPane.showInputDialog("Digite Nombre");
-					String apellido = JOptionPane.showInputDialog("Digite Apellido");
-					double salario = Double.parseDouble(JOptionPane.showInputDialog("Digite salario"));
+		int numero = -1;
+		try {
+			while(numero != 0) {
+				numero = Integer.parseInt(JOptionPane.showInputDialog("Digite lo que desea" + "\n" + "0: Salir                                " + "             1: Agregar Empleado"
+						+ "\n" + "2: Eliminar empleado      "+ "             3: Modificar Empleado" + "\n" + "4: Mostrar Empleados    " + "             5: Empleado mayor salario"
+								+ "\n" + "6: Empleado menor salario        "+ "7: Ordenar por nombre la lista" + "\n" + "8: Total de Apellidos por 'A'        " +"9: Cinco empleados con mayor salario" + "\n" + "10: Sumatoria salarios mayores a 700k"));
+		
+				switch (numero) {
+				case 1:
+					try {
+						int id = Integer.parseInt(JOptionPane.showInputDialog("Digite id"));
+						String nombre = JOptionPane.showInputDialog("Digite Nombre");
+						String apellido = JOptionPane.showInputDialog("Digite Apellido");
+						double salario = Double.parseDouble(JOptionPane.showInputDialog("Digite salario"));
+						
+						if(p.agregar(id, nombre, apellido, salario)) {
+							JOptionPane.showMessageDialog(null, "Agregó");
+						}
+						break;
+					}
+					catch (Exception e) {
+						JOptionPane.showMessageDialog(null, e.getMessage());
+					}
+					break;
 					
-					if(p.agregar(id, nombre, apellido, salario)) {
-						JOptionPane.showMessageDialog(null, "Agregó");
+				case 2:
+					try {
+						int id = Integer.parseInt(JOptionPane.showInputDialog("Digite id"));
+						if (p.eliminar(id)) {
+							JOptionPane.showMessageDialog(null, "Eliminó");
+						}
+						break;
+					}
+					catch (Exception e) {
+						JOptionPane.showMessageDialog(null, e.getMessage());
 					}
 					break;
-				}
-				catch (Exception e) {
-					JOptionPane.showMessageDialog(null, e.getMessage());
-				}
-				break;
-				
-			case 2:
-				try {
-					int id = Integer.parseInt(JOptionPane.showInputDialog("Digite id"));
-					if (p.eliminar(id)) {
-						JOptionPane.showMessageDialog(null, "Eliminó");
-					}
-					break;
-				}
-				catch (Exception e) {
-					JOptionPane.showMessageDialog(null, e.getMessage());
-				}
-				break;
-				
-			case 3:
-				try {
-					int id = Integer.parseInt(JOptionPane.showInputDialog("Digite id"));
-					String nombre = JOptionPane.showInputDialog("Digite Nombre");
-					String apellido = JOptionPane.showInputDialog("Digite Apellido");
-					double salario = Double.parseDouble(JOptionPane.showInputDialog("Digite salario"));
 					
-					if(p.modificar(id, nombre, apellido, salario)) {
-						JOptionPane.showMessageDialog(null, "Modificó");
+				case 3:
+					try {
+						int id = Integer.parseInt(JOptionPane.showInputDialog("Digite id"));
+						String nombre = JOptionPane.showInputDialog("Digite Nombre");
+						String apellido = JOptionPane.showInputDialog("Digite Apellido");
+						double salario = Double.parseDouble(JOptionPane.showInputDialog("Digite salario"));
+						
+						if(p.modificar(id, nombre, apellido, salario)) {
+							JOptionPane.showMessageDialog(null, "Modificó");
+						}
+						break;
+					}
+					catch (Exception e) {
+						JOptionPane.showMessageDialog(null, e.getMessage());
 					}
 					break;
-				}
-				catch (Exception e) {
-					JOptionPane.showMessageDialog(null, e.getMessage());
-				}
-				break;
-			
-			case 4:
-				try {
-					JOptionPane.showMessageDialog(null, p.mostrar());
-					break;
-				}
-				catch (Exception e) {
-					JOptionPane.showMessageDialog(null, e.getMessage());
-				}
-				break;
 				
-			case 5:
-				break;
+				case 4:
+					try {
+						JOptionPane.showMessageDialog(null, p.mostrar());
+						break;
+					}
+					catch (Exception e) {
+						JOptionPane.showMessageDialog(null, e.getMessage());
+					}
+					break;
+					
+				case 5:
+					try {
+						JOptionPane.showMessageDialog(null, p.empleadoConMayorSalario());
+					} catch (Exception e) {
+						
+					}
+					break;
+					
+				case 6:
+					try {
+						JOptionPane.showMessageDialog(null, p.empleadoConMenorSalario());
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Algo salio mal");
+					}
+					break;
+						
+				case 7:
+					try {
+						JOptionPane.showMessageDialog(null, p.ordenarPorNombre());
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Algo salio mal");
+					}
+					break;
+					
+				case 8:
+					try {
+						JOptionPane.showMessageDialog(null, p.mostrarNumeroTotalDeNombresPorA());
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Algo salio mal");
+					}
+					break;
+				
+				case 9:
+					try {
+						JOptionPane.showMessageDialog(null, p.cincoEmpleadosConMayorSalario());
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Algo salio mal");
+					}
+					break;
+					
+				case 10: 
+					try {
+						JOptionPane.showMessageDialog(null, p.sumaSalariosMayorSiete());
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Algo salio mal");
+					}
+				}
 			}
 		}
-
+		catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Uy! revisa que hiciste amigo");
+		}
 	}
 
 }
